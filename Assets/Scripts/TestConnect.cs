4df5c021dd8679bc074cb6aa0d839043ae68da46
Henry <193541T@mymail.nyp.edu.sg>
2020-11-28 08:03:35 +0800

    Connect to Photon

---
+++ Assets/Scripts/TestConnect.cs
@@ -0,0 +1,40 @@
+﻿//Copyright (c) Ling Guan Yu (193541T, NYP SIDM GDT 1904)
+
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class TestConnect: MonoBehaviourPunCallbacks {
+    #region Fields
+    #endregion
+
+    #region Properties
+    #endregion
+
+    #region Ctors and Dtor
+
+    public TestConnect():
+        base()
+    {
+    }
+
+    #endregion
+
+    #region Unity User Callback Event Funcs
+
+    private void Start() {
+        print("Connecting to server...");
+        PhotonNetwork.GameVersion = "4.0";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    #endregion
+
+    public override void OnConnectedToMaster() {
+        print("Connected to server");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause) {
+        print("Disconnected to server: " + cause.ToString());
+    }
+}
