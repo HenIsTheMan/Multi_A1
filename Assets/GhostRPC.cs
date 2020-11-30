499a9ef2feb0be9db8cd4308feaace34e1b1bf58
Henry <193541T@mymail.nyp.edu.sg>
2020-11-30 16:00:36 +0800

    Looks gd as a start

---
+++ Assets/GhostRPC.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class GhostRPC: MonoBehaviour{
+	[PunRPC]
+	public void CallCaughtPlayer(){
+		GetComponentInChildren<Observer>().CaughtPlayer();
+	}
+}
