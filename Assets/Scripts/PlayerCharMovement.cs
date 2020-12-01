a5a43363876322944362cd8e5ded09b4c1e382b8
Henry <193541T@mymail.nyp.edu.sg>
2020-12-01 16:52:10 +0800

    Player char movement

---
+++ Assets/Scripts/PlayerCharMovement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System;
+
+internal class PlayerCharMovement: MonoBehaviour {
+	#region Fields
+
+	private bool isKillButtonPressed;
+	private float horizAxis;
+	private float vertAxis;
+	private Rigidbody rigidbodyComponent;
+
+	[SerializeField] private float spd;
+
+	#endregion
+
+	#region Properties
+	#endregion
+
+	#region Ctors and Dtor
+
+	public PlayerCharMovement() {
+		isKillButtonPressed = false;
+		horizAxis = 0.0f;
+		vertAxis = 0.0f;
+		rigidbodyComponent = null;
+
+		spd = 0.0f;
+	}
+
+	#endregion
+
+	#region Unity User Callback Event Funcs
+
+	private void Start() {
+		rigidbodyComponent = GetComponent<Rigidbody>();
+	}
+
+	private void Update() {
+		if(Input.GetButtonDown("Kill")) {
+			isKillButtonPressed = true;
+		}
+		horizAxis = Input.GetAxisRaw("Horizontal");
+		vertAxis = Input.GetAxisRaw("Vertical");
+	}
+
+	private void FixedUpdate() {
+		rigidbodyComponent.velocity = new Vector3(horizAxis, vertAxis, 0.0f).normalized * spd * Time.fixedDeltaTime;
+
+		if(isKillButtonPressed) {
+			//Kill logic??
+
+			isKillButtonPressed = false;
+		}
+	}
+
+	#endregion
+}
