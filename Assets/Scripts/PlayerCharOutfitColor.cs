1aadc4250bf96fb3324d7fd0de1d4455ca3e5d8c
Henry <193541T@mymail.nyp.edu.sg>
2020-12-01 23:35:17 +0800

    Set outfit color to red

---
+++ Assets/Scripts/PlayerCharOutfitColor.cs
@@ -0,0 +1,42 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+namespace Impasta.Game{
+    internal sealed class PlayerCharOutfitColor: MonoBehaviour {
+        #region Fields
+
+        private PhotonView photonView;
+        private SpriteRenderer childSpriteRenderer;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        public PlayerCharOutfitColor() {
+            photonView = null;
+            childSpriteRenderer = null;
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Awake() {
+            childSpriteRenderer = gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>();
+            UnityEngine.Assertions.Assert.IsNotNull(childSpriteRenderer);
+
+            photonView = gameObject.GetComponent<PhotonView>();
+            UnityEngine.Assertions.Assert.IsNotNull(photonView);
+        }
+
+        private void Start() {
+            //childSpriteRenderer.color = JLGame.GetColor(photonView.Owner.GetPlayerNumber());
+            childSpriteRenderer.color = Color.red;
+        }
+
+        #endregion
+    }
+}
