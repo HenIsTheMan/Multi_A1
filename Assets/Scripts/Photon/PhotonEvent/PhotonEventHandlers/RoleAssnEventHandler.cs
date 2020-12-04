7ead2147a58acf6e3ca7fd89403c8cb05f281f08
Henry <193541T@mymail.nyp.edu.sg>
2020-12-04 13:04:11 +0800

    Red name for imposter

--- Assets/Scripts/Photon/PhotonEvent/PhotonEventHandlers/RoleAssnEventHandler.cs
+++ Assets/Scripts/Photon/PhotonEvent/PhotonEventHandlers/RoleAssnEventHandler.cs
@@ -39,9 +39,26 @@ namespace Impasta.Game {
 
                     gameObject.GetComponent<PlayerCharKill>().IsImposter = isImposter;
 
-                    Text textComponent = GameObject.Find("PlayerNameText").GetComponent<Text>();
+                    Transform childTransform = gameObject.transform.Find("PlayerNameCanvas");
+                    Transform grandchildTransform = childTransform.Find("PlayerNameText");
+                    Text textComponent = grandchildTransform.GetComponent<Text>();
+
                     textComponent.text = PhotonNetwork.LocalPlayer.NickName;
-                    //childTextComponent.color
+
+                    GameObject[] playerCharGOs = GameObject.FindGameObjectsWithTag("Player");
+                    int arrLen = playerCharGOs.Length;
+                    for(int i = 0; i < arrLen; ++i) {
+                        if(isImposter){
+                            textComponent.color = Color.red;
+
+                            GameObject playerCharGO = playerCharGOs[i];
+                            if(playerCharGO != gameObject && playerCharGO.GetComponent<PlayerCharKill>().IsImposter) {
+                                Transform playerCharGOChildTransform = gameObject.transform.Find("PlayerNameCanvas");
+                                Transform playerCharGOGrandchildTransform = childTransform.Find("PlayerNameText");
+                                playerCharGOGrandchildTransform.GetComponent<Text>().color = Color.red;
+                            }
+                        }
+                    }
 
                     break;
             }
