1466cfa5e506bb4ab6c40c3da08a347b005bff5c
Henry <193541T@mymail.nyp.edu.sg>
2020-12-06 11:02:22 +0800

    Just in case

---
+++ Assets/Scripts/Game/Report/SendMsg.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Impasta.Game {
+    internal sealed class SendMsg: MonoBehaviour {
+        #region Fields
+
+        [SerializeField] private Text msg;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        public SendMsg() {
+            msg = null;
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+        #endregion
+
+        public void OnSendButtonPressed() {
+            msg.text = string.Empty;
+        }
+    }
+}
