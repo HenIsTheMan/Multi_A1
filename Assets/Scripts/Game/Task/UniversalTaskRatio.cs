76e200f2b6cc34e318f44d55c4ea3d1ff1f13463
Henry <193541T@mymail.nyp.edu.sg>
2020-12-07 13:55:58 +0800

    UniversalTaskRatio.cs

---
+++ Assets/Scripts/Game/Task/UniversalTaskRatio.cs
@@ -0,0 +1,50 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Impasta.Game {
+    internal sealed class UniversalTaskRatio: MonoBehaviour {
+        #region Fields
+
+        [SerializeField] private Text textComponent;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        public UniversalTaskRatio() {
+            textComponent = null;
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Update() {
+            GameObject tagObj = (GameObject)PhotonNetwork.LocalPlayer.TagObject;
+            if(tagObj != null && !tagObj.GetComponent<PlayerCharKill>().IsImposter) {
+                GameObject[] playerChars = GameObject.FindGameObjectsWithTag("Player");
+                int completeTasksSum = 0;
+                int tasksSum = 0;
+                int arrLen = playerChars.Length;
+
+                for(int i = 0; i < arrLen; ++i) {
+                    GameObject playerChar = playerChars[i];
+                    if(!playerChar.GetComponent<PlayerCharKill>().IsImposter) {
+                        PlayerCharTasks playerCharTasks = playerChar.GetComponent<PlayerCharTasks>();
+                        completeTasksSum += playerCharTasks.AmtOfCompleteTasks;
+                        tasksSum += playerCharTasks.TotalAmtOfTasks;
+                        break;
+                    }
+                }
+
+                textComponent.text = completeTasksSum.ToString() + '/' + tasksSum;
+            }
+        }
+
+        #endregion
+    }
+}
