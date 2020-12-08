d0a20d3ee1b43c1db36a6a5e0ddc8fbcb3265e99
Henry <193541T@mymail.nyp.edu.sg>
2020-12-08 08:18:32 +0800

    ButtonTaskBlock

---
+++ Assets/Scripts/Game/Task/ButtonTaskBlock.cs
@@ -0,0 +1,29 @@
+﻿namespace Impasta.Game {
+    internal sealed class ButtonTaskBlock: TaskBlock {
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtzor
+
+        private ButtonTaskBlock(): base() {
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+		private new void Update() => base.Update();
+
+        #endregion
+
+        protected override void TaskLogic() {
+        }
+
+        public void OnButtonClick() {
+            TaskCompleted();
+        }
+    }
+}
