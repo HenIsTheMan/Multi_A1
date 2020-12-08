7657a8570811bd7efb3b8880bbb59caa4b9180eb
Henry <193541T@mymail.nyp.edu.sg>
2020-12-08 08:37:51 +0800

    Faster

---
+++ Assets/Scripts/Game/Task/PressAndHoldTaskBlock.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Impasta.Game {
+    internal sealed class PressAndHoldTaskBlock: TaskBlock {
+        #region Fields
+
+        private bool hold;
+        private float holdTime;
+
+        private Text textComponent;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtzor
+
+        private PressAndHoldTaskBlock() : base() {
+            hold = false;
+            holdTime = 0.0f;
+
+            textComponent = null;
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private new void Update() => base.Update();
+
+        #endregion
+
+        private void Awake() {
+            holdTime = Random.Range(7.0f, 10.0f);
+
+            textComponent = taskCanvasGO.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Text>();
+        }
+
+        protected override void TaskLogic() {
+            if(hold) {
+                textComponent.text = Mathf.Ceil(holdTime).ToString();
+                holdTime -= Time.deltaTime;
+
+                if(holdTime <= 0.0f) {
+                    TaskCompleted();
+                }
+            }
+        }
+
+        public void OnHold() {
+            hold = true;
+        }
+
+        public void OnRelease() {
+            hold = false;
+        }
+    }
+}
